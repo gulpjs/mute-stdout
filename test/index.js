8c643e1764d88563d981d401699944264b264e24
sttk <sttk.xslet@gmail.com>
2018-08-03 19:34:48 +0900

    Fix the issue that the test fails on node v9 and later

--- test/index.js
+++ test/index.js
@@ -1,61 +1,24 @@
 'use strict';
 
-var stream = require('stream');
-
 var lab = exports.lab = require('lab').script();
 var code = require('code');
 
 var stdout = require('../');
 
-lab.describe('mute', function(){
+lab.describe('mute-stdout', function(){
 
-  var ogWrite = stream.Duplex.prototype.write;
+  var ogWrite = process.stdout.write;
 
-  lab.it('mutes the stream', function(done){
-
-    var writes = 0;
-
-    stream.Duplex.prototype.write = function(){
-      writes++;
-    };
+  lab.it('mute and unmute ', function(done){
+    code.expect(process.stdout.write).to.equal(ogWrite);
 
     stdout.mute();
-
-    console.log('should not print');
-
-    stdout.unmute();
-    stream.Duplex.prototype.write = ogWrite;
-
-    code.expect(writes).to.equal(0);
-
-    done();
-  });
-});
-
-lab.describe('unmute', function(){
-
-  var ogWrite = stream.Duplex.prototype.write;
-
-  lab.it('unmutes a muted stream', function(done){
-
-    var writes = 0;
-
-    stream.Duplex.prototype.write = function(){
-      writes++;
-    };
-
-    stdout.mute();
-
-    console.log('should not print');
-
+    var mutedWrite = process.stdout.write;
     stdout.unmute();
 
-    console.log('should print');
-
-    stream.Duplex.prototype.write = ogWrite;
-
-    code.expect(writes).to.equal(1);
-
+    code.expect(mutedWrite).to.not.equal(ogWrite);
+    code.expect(mutedWrite.toString()).to.equal('function noop(){}');
+    code.expect(process.stdout.write).to.equal(ogWrite);
     done();
   });
 
